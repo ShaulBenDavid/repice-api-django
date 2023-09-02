71c0f14b06052f5afb515ed2ee4873db14414322
Shaul Ben david <Shaulbd9@gmail.com>
2023-09-02 15:10:25 +0300

    conifuring database

---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+Calculator functions
+"""
+from django.test import SimpleTestCase
+
+from app.calc import add, subtract
+
+
+class ClacTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = subtract(10, 15)
+
+        self.assertEqual(res, 5)
+
