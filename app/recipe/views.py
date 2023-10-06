9d68cafdcc93a621c08c7d5757377701431038e7
Shaul Ben david <Shaulbd9@gmail.com>
2023-10-06 11:57:27 +0300

    lint fix

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -26,7 +26,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
 
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
